3d8e8b89d4d2129e6eb8fbb911e847f4cde2ec02
Kreshnik Mati <kmati@kmati.local>
2016-03-04 11:36:42 -0500

    trying to fix packages and stuff

--- general.el
+++ general.el
@@ -1,11 +1,12 @@
-
+;;; package --- Summary
 
 ;; Set the hyper key to be the fn button
 ;;(setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
 
 ;; move border!
-(load "~/.emacs.d/vendor/move-border" )
+(load "~/.emacs.d/personal/vendor/move-border")
 (require 'move-border)
+;;; Code
 (global-set-key (kbd "C-s-<up>") 'move-border-up)
 (global-set-key (kbd "C-s-<down>") 'move-border-down)
 (global-set-key (kbd "C-s-<left>") 'move-border-left)
@@ -18,13 +19,14 @@
 (global-set-key (kbd "H-x") 'next-buffer)
 (global-set-key (kbd "H-z") 'previous-buffer)
 (global-set-key (kbd "H-b") 'list-buffers)
-(global-set-key (kbd "H-c") 'scroll-lock-mode)
+(
+ global-set-key (kbd "H-c") 'scroll-lock-mode)
 (global-set-key (kbd "H-k") 'kill-buffer)
 (global-set-key (kbd "H-f") 'find-file)
 
 
 ;; Define - Redo shortcut
-(global-set-key (kbd "s-Z") 'undo-tree-redo) . (Z = SHIFT + z).
+(global-set-key (kbd "s-Z") 'undo-tree-redo)
 
 ;; Clear Read-Only Buffers
 (defun comint-clear-buffer ()
@@ -34,5 +36,3 @@
 
 ;; let's bind the new command to a keycombo
 (define-key comint-mode-map "\C-c\M-o" #'comint-clear-buffer)
-
-
---
+++ preload/package.el
@@ -0,0 +1,31 @@
+(require 'package)
+
+
+;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;;(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("popkit" . "http://elpa.popkit.org/packages/"))
+(package-refresh-contents)
+(package-initialize)
+
+(if (not (package-installed-p 'use-package))
+   (progn
+     (package-refresh-contents)
+     (package-install 'use-package)))
+
+(require 'use-package)
+(package-refresh-contents)
+(package-initialize)
+
+;;; Expand-region
+(use-package fsharp-mode
+   :ensure fsharp-mode)
+
+(use-package neotree
+   :ensure neotree)
+
+;(use-package
+;   :ensure )
