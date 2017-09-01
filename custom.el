13e6bfa5e818bf347d84459066222ba79906b661
Hak Nick <haknick@gmail.com>
2017-09-01 06:54:19 -0400

    custom

--- custom.el
+++ custom.el
@@ -1,3 +1,7 @@
+
+
+(defconst indent-val 4 )
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -5,22 +9,29 @@
  ;; If there is more than one, they won't work right.
  '(initial-scratch-message
    ";; This can't be saved, but can evaluate Lisp.
-;; If you want to create a file, visit that file with C-x C-f,
-;; To check and change any config/command C-h v
-;; ...
+    ;; If you want to create a file, visit that file with C-x C-f,
+    ;; To check and change any config/command C-h v
+    ;; ..."
+  )
 
-")
- '(js-indent-level 2)
- '(js2-basic-offset 2)
- '(neo-show-hidden-files t)
- '(tab-width 2))
+ ;; Setup indent levels
+ '(js-indent-level indent-val)
+ '(js2-basic-offset indent-val)
+ '(tab-width indent-val)
+
+ ;; finish custom-set-variables
+ )
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset indent-val)))
+
+;; Do now show neotree hidden files
+'(neo-show-hidden-files t)
+
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
